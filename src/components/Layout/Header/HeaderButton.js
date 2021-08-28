4855168d52fc8073f06c2062d541bbc8c2cb0386
feward-alicankilic <kilic@feward.com>
2021-08-28 21:06:48 +0300

    Finished

--- src/components/Layout/Header/HeaderButton.js
+++ src/components/Layout/Header/HeaderButton.js
@@ -1,24 +1,42 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import styles from "./HeaderButton.module.css";
 import CartIcon from "./CartIcon";
 import CartContext from "../../../store/cart-context";
 
-
 const HeaderButton = (props) => {
-
-  const {click} = props;
+  const { click } = props;
+  const [btnState, setbtnState] = useState(false);
 
   const CartCtx = useContext(CartContext);
-  const numberOfCartItems = CartCtx.items.reduce((curNumber,item) => { return curNumber+item.amount}, 0 );
+  const { items } = CartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  const buttonClasses = `${styles.button} ${btnState ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnState(true);
+    const Timer = setTimeout(() => {
+      setbtnState(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(Timer);
+    };
+  }, [items]);
+
   return (
     <Fragment>
-      <button className={styles.button} onClick={click}>
+      <button className={buttonClasses} onClick={click}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={styles.badge}>{numberOfCartItems}</span>
-      </button >
+      </button>
     </Fragment>
   );
 };
