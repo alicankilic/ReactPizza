31d3dfde0a66ac709a9087d20883a53f47803c82
feward-alicankilic <kilic@feward.com>
2021-08-28 19:50:12 +0300

    updated items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,6 @@
-import React, { useReducer } from "react";
-import CartContext from "./cart-context";
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
@@ -7,15 +8,53 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedNewTotalAmount =
+  if (action.type === 'ADD') {
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
-      totalAmount: updatedNewTotalAmount,
+      totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
   return defaultCartState;
 };
 
@@ -25,19 +64,21 @@ const CartProvider = (props) => {
     defaultCartState
   );
 
-  const addItemToCart = (item) => {
-    dispatchCartAction({ type: "ADD", item: item });
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
   };
-  const removeItemFromCart = (id) => {
-    dispatchCartAction({ type: "REMOVE ", id: id });
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
-    addItem: addItemToCart,
-    removeItem: removeItemFromCart,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
